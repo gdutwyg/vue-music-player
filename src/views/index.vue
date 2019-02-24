ca7132fdd710badbb801df98cc25bf17951e092c
wangyg <>
2019-02-24 08:40:07 +0800

    重命名组件，增加图标

---
+++ src/views/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="music-box">
+    <header class="music-header">
+      <h1 class="music-header-title">简易音乐播放器</h1>
+      <div class="music-login-button">登录</div>
+    </header>
+    <div class="music-content">
+      <div class="music-list">
+        <div class="music-list-header">
+          <span class="music-header-name">歌名</span>
+          <span class="music-header-author">歌手</span>
+        </div>
+        <ul class="music-list-content">
+          <li class="music-list-item" @click="turnDetail">
+            <span class="music-item-num">1</span>
+            <span class="music-item-name">xxx</span>
+            <!-- <span class="music-item-play-icon"></span> -->
+            <span class="music-item-author">xxx/abc</span>
+          </li>
+          <li class="music-list-item">
+            <span class="music-item-num">1</span>
+            <span class="music-item-name">xxx</span>
+            <!-- <span class="music-item-play-icon"></span> -->
+            <span class="music-item-author">xxx/abc</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="music-footer"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'index',
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {
+    turnDetail() {
+      this.$router.push('detail')
+    }
+  }
+}
+</script>
+<style lang="less">
+.music-box {
+  width: 100%;
+  height: 100%;
+}
+.music-header {
+  display: flex;
+  height: 60px;
+  background: rgb(229, 71, 60);
+  color: #fff;
+  justify-content: space-between;
+  align-items: center;
+  .music-header-title {
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: 20px;
+  }
+  .music-login-button {
+    margin-right: 20px;
+    padding: 5px 8px;
+    border: 1px solid #fff;
+    border-radius: 5px;
+  }
+}
+.music-content {
+  width: 100%;
+  height: calc(100% - 80px - 60px);
+  background: rgb(#000, 0.6);
+  color: #fff;
+  .music-list {
+    padding: 0 12px;
+    .music-list-header {
+      display: flex;
+      height: 50px;
+      align-items: center;
+      border-bottom: 1px solid hsla(0, 0%, 100%, 0.2);
+      .music-header-name {
+        flex: 1;
+        margin-left: 40px;
+      }
+      .music-header-author {
+        float: right;
+        width: 100px;
+      }
+    }
+    .music-list-item {
+      display: flex;
+      width: 100%;
+      height: 50px;
+      line-height: 50px;
+      overflow: hidden;
+      border-bottom: 1px solid hsla(0, 0%, 100%, 0.2);
+      .music-item-num {
+        width: 30px;
+        margin-left: 10px;
+      }
+      .music-item-name {
+        flex: 1;
+      }
+      .music-item-play-icon {
+        width: 50px;
+        height: 50px;
+        background-image: url(../assets/imgs/player/pause.png);
+        background-size: contain;
+      }
+      .music-item-author {
+        width: 100px;
+      }
+    }
+  }
+}
+</style>

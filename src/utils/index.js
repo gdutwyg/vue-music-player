8cbc099b2aed156ccccd977ceabc3632a0f8cc3b
wangyuguo <wangyuguo@xndm.tech>
2019-06-28 15:29:14 +0800

    限制图片的大小为300*300，加载更快

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 class Utils {
-  formatPlayList(list) {
+  formatPlayList (list) {
     if (!list.length) return []
     return list.map(item => {
       const singer = this.formatSinger(item.ar)
@@ -9,18 +9,18 @@ class Utils {
         name: item.name,
         singer: singer,
         album: item.al.name,
-        picUrl: item.al.picUrl,
+        picUrl: `${item.al.picUrl}?param=300y300`,
         duration: duration,
         musicTime: this.formatMusicTime(duration),
         url: `https://music.163.com/song/media/outer/url?id=${item.id}.mp3`
       }
     })
   }
-  formatSinger(ar) {
+  formatSinger (ar) {
     if (!ar.length) return '佚名'
     return ar.map(item => item.name).join('/')
   }
-  formatMusicTime(duration) {
+  formatMusicTime (duration) {
     let ss = parseInt(duration) // 秒
     let mm = 0 // 分
     let hh = 0 // 小时

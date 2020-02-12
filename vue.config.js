f04394c88948a97e59e530df082f38bbbedf4deb
wangyg <13660110512@163.com>
2020-02-12 11:00:51 +0800

    更新vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: '/vue-music-player/',
-  productionSourceMap: false,  // 生产环境禁用
+  productionSourceMap: false, // 生产环境禁用
   configureWebpack: {
     devtool: false // 开发环境禁用
   },
